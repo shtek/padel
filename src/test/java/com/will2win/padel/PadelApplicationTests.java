90ba477dcd43b70005ae969cfa9fd452271f6062
shtek <shtek@yahoo.com>
2022-05-31 01:24:06 +0100

    initial commit

---
+++ src/test/java/com/will2win/padel/PadelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.will2win.padel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PadelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
